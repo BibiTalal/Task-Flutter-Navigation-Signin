2d0200c86bf72ff1875b5e32d1a8b542f524b578
BibiTalal <bibi.t_alfahad@hotmail.com>
2022-11-27 17:35:28 +0300

    task completed

--- lib/pages/signed_in.dart
+++ lib/pages/signed_in.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 class SignedIn extends StatelessWidget {
-  // Step 5
-  const SignedIn({
-    Key? key,
-  }) : super(key: key);
-  // Step 6
+  SignedIn({required this.username, Key? key}) : super(key: key);
+
+  final String username;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +15,8 @@ class SignedIn extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            // Step 9
-            Text("Welcome username"),
+          children: [
+            Text("Welcome $username"),
             Icon(
               Icons.check_circle,
               color: Colors.green,
